37b393dc5bd19c498357932853b07356ea6f5319
yurism <mascarenhasyuri@hotmail.com>
2024-02-26 22:03:46 -0300

    [Refac] change class to newly defined variable

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
       <div className="flex items-center w-full md:2/3">
-        <Link href="/" className="font-black text-[#F27405]">
+        <Link href="/" className="font-black text-tertiary-dark">
           Hotelz
         </Link>
         <ul className="flex items-center ml-5">
