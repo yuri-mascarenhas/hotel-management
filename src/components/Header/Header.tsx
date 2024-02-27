d2f0ec23a3b8aa37c98a9c7dc0561042575a8f37
yurism <mascarenhasyuri@hotmail.com>
2024-02-26 21:54:56 -0300

    [Add] utilities links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,6 +20,17 @@ const Header = () => {
           </li>
         </ul>
       </div>
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/rooms">Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
     </header>
   );
 };
