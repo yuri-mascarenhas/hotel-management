65ab27021361e52cbe5a69bbd25054054b58e896
yurism <mascarenhasyuri@hotmail.com>
2024-02-26 21:36:27 -0300

    [Add] Initial header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3"></div>
+      Header
+    </header>
+  );
+};
+
+export default Header;
