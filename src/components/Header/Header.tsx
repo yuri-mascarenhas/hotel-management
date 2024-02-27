0fd3fdd9e8ed42c62711e04c2d9a1091df9ba070
yurism <mascarenhasyuri@hotmail.com>
2024-02-26 21:42:05 -0300

    [Add] Link to home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
-      <div className="flex items-center w-full md:2/3"></div>
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/" className="font-black text-[#F27405]">
+          Hotelz
+        </Link>
+        <ul className="flex items-center ml-5"></ul>
+      </div>
       Header
     </header>
   );
